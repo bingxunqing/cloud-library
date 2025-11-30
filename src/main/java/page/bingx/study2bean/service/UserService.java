3cc1eea275a9e18ac31925fffd75edafc8a9ab6f
Qiu Hongming <8208241214@csu.edu.cn>
2025-11-30 16:53:04 +0800

    初始化项目

--- src/main/java/page/bingx/study2bean/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package page.bingx.study2bean.service;
-
-import page.bingx.study2bean.pojo.User;
-import page.bingx.study2bean.vo.UserVO;
-
-public interface UserService {
-    UserVO getUserWithOrders(Integer id);
-//    int register(User user);
-}
